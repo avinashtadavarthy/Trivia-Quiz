bd6b3e3360a2becaa921c64b722369e0e3cfb140
Avinash Tadavarthy <avinash.tadavarthy@gmail.com>
2020-07-23 22:29:04 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <div id="app">
+      <Header 
+        :numCorrect="numCorrect"
+        :numTotal="numTotal"
+      />
+      <QuestionBox
+        v-if="questions.length"
+        :currentQuestion="questions[index]"
+        :next="next"
+        :increment="increment"
+      />
+    </div>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++;
+    }
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=30&type=multiple", {
+      method: "get"
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
   }
-}
+};
 </script>
 
 <style>
